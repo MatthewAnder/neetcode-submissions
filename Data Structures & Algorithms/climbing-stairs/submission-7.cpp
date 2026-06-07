cb3a765ed0b5ea91055a57224a429d3b95f85b71
Matthew Andersen Haryanto <93026560+MatthewAnder@users.noreply.github.com>
2026-06-06 17:33:37 -0700

    Add: climbing-stairs - submission-7

---
+++ Data Structures & Algorithms/climbing-stairs/submission-7.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        vector<int> dp(n+1, 0);
+        dp[1] = 1;
+        if (n <= 1)
+            return dp[n];
+        dp[2] = 2;
+
+        for (size_t i = 3; i <= n; i++) {
+            dp[i] = dp[i-2] + dp[i-1];
+        }
+        
+        return dp[n];
+    }
+};
